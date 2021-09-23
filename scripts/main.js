f489d1913cea6d78167a0fc07c932db4a12d24e2
Kyon Prom <8162402@isd535.org>
2021-09-23 09:43:19 -0500

    my game

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
-// Put your JavaScript here
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+let rightPressed = false;
+let leftPressed = false;
+
+function draw() {
+    //draw the player
+    ctx.beginPath();
+    ctx.arc(350, 350, 30, 0, Math.PI*2);
+    ctx.fillStyle = "purple"
+    ctx.fill();
+    ctx.closePath();
+}
+function keyDownHandler(e){
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if
+    }
+}
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+setInterval(draw, 10);
