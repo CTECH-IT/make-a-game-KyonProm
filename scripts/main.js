a34bc0a36565a06bafb8651af7e8cfe4318515da
Kyon Prom <8162402@isd535.org>
2021-09-24 09:38:46 -0500

    set up

--- scripts/main.js
+++ scripts/main.js
@@ -4,21 +4,49 @@ let ctx = canvas.getContext("2d");
 let rightPressed = false;
 let leftPressed = false;
 
+let playerHeight = 30;
+let playerWidth = 30;
+let playerX = (30) /2;
+
 function draw() {
-    //draw the player
-    ctx.beginPath();
-    ctx.arc(350, 350, 30, 0, Math.PI*2);
-    ctx.fillStyle = "purple"
-    ctx.fill();
-    ctx.closePath();
+    
+    function drawPlayer() {
+        ctx.beginPath();
+        ctx.arc(350, 350, playerWidth, 0, Math.PI*2);
+        ctx.fillStyle = "blue"
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    // player movement 
+    if(rightPressed) {
+        playerX +- 7;
+    }
+    else if(leftPressed) {
+        playerX -= 7;
+    }
+    drawPlayer();
+
 }
-function keyDownHandler(e){
+
+function keyDownHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = true;
     }
-    else if
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
     }
 }
+
+function keyUpHandler(e){
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+      leftPressed = false;
+    }
+}
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
